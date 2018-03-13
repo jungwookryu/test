edf9b9bdad4119ac5348e116a3d034cdf619623a
injeongLee <ijlee@hdtel.co.kr>
2018-03-13 09:27:53 +0900

    rabbitMQ주석후 commit

--- src/main/java/com/ht/connected/home/backend/config/service/MqttConfig.java
+++ src/main/java/com/ht/connected/home/backend/config/service/MqttConfig.java
@@ -1,56 +1,73 @@
-package com.ht.connected.home.backend.config.service;
+/*package com.ht.connected.home.backend.config.service;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.core.MessageProducer;
-import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
-import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
-import org.springframework.messaging.MessagingException;
 
 @Configuration
 @PropertySource("classpath:/mqttConfig.properties")
 public class MqttConfig {
+	final static String queueName = "ht-rabbitMQ";
 
 	@Bean
-	public MessageChannel mqttInputChannel() {
-		return new DirectChannel();
+	Queue queue() {
+		return new Queue(queueName, false);
 	}
 
 	@Bean
-	public MessageProducer inbound() {
-		MqttPahoMessageDrivenChannelAdapter adapter = 
-				new MqttPahoMessageDrivenChannelAdapter("tcp://192.168.2.112:1883",
-				"guest", "/hcs-w1001/abcd/#", "{\r\n" + 
-						"    \"user_email\": \"k@k.com\",\r\n" + 
-						"    \"serialNo\": \"01D0014\",\r\n" + 
-						"    \"nodeId\":6,\r\n" + 
-						"    \"endpointId\":0,\r\n" + 
-						"    \"option\":\"none\"\r\n" + 
-						"}\r\n" + 
-						"");
-		adapter.setCompletionTimeout(5000);
-		adapter.setConverter(new DefaultPahoMessageConverter());
-		adapter.setQos(1);
-		adapter.setOutputChannel(mqttInputChannel());
-		return adapter;
+	TopicExchange exchange() {
+		return new TopicExchange("spring-boot-exchange");
 	}
 
 	@Bean
-	@ServiceActivator(inputChannel = "mqttInputChannel")
-	public MessageHandler handler() {
-		return new MessageHandler() {
-
-			@Override
-			public void handleMessage(Message<?> message) throws MessagingException {
-				System.out.println(message.getPayload());
-			}
-
-		};
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(queueName);
 	}
-}
+
+	@Bean
+	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+			MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	
+	 * @Bean public MessageChannel mqttInputChannel() { return new DirectChannel();
+	 * }
+	 * 
+	 * @Bean public MessageProducer inbound() { MqttPahoMessageDrivenChannelAdapter
+	 * adapter = new MqttPahoMessageDrivenChannelAdapter("tcp://192.168.2.112:1883",
+	 * "guest", "/hcs-w1001/abcd/#", "{\r\n" +
+	 * "    \"user_email\": \"k@k.com\",\r\n" + "    \"serialNo\": \"01D0014\",\r\n"
+	 * + "    \"nodeId\":6,\r\n" + "    \"endpointId\":0,\r\n" +
+	 * "    \"option\":\"none\"\r\n" + "}\r\n" + "");
+	 * adapter.setCompletionTimeout(5000); adapter.setConverter(new
+	 * DefaultPahoMessageConverter()); adapter.setQos(1);
+	 * adapter.setOutputChannel(mqttInputChannel()); return adapter; }
+	 * 
+	 * @Bean
+	 * 
+	 * @ServiceActivator(inputChannel = "mqttInputChannel") public MessageHandler
+	 * handler() { return new MessageHandler() {
+	 * 
+	 * @Override public void handleMessage(Message<?> message) throws
+	 * MessagingException { System.out.println(message.getPayload()); }
+	 * 
+	 * }; }
+	 
+}*/
---
+++ src/main/java/com/ht/connected/home/backend/service/impl/base/RabbitMQService.java
@@ -0,0 +1,6 @@
+package com.ht.connected.home.backend.service.impl.base;
+
+public class RabbitMQService {
+	private final static String QUEUE_NAME = "hello";
+
+}
