f01218d23d38447a56a0a213bd32cb437bf90246
ht-jhkoo <jhkoo@hdtel.co.kr>
2018-03-27 14:49:10 +0900

    zwave 요청에 대한 서비스 인터페이스

--- src/main/java/com/ht/connected/home/backend/service/ZwaveService.java
+++ src/main/java/com/ht/connected/home/backend/service/ZwaveService.java
@@ -6,8 +6,20 @@ import com.ht.connected.home.backend.model.dto.ZwaveRequest;
 import com.ht.connected.home.backend.model.entity.Certification;
 import com.ht.connected.home.backend.service.base.CrudService;
 
+/**
+ * zwave 서비스 인터페이스 
+ * @author 구정화
+ *
+ */
 public interface ZwaveService extends CrudService<Certification, Integer>{
 
+	/**
+	 * zwave 인증과 서비스 앱 프로토몰 공통 사용
+	 * @param req
+	 * @param zwaveCommonRequest
+	 * @param b 인증프로토몰일경우 true
+	 * @return
+	 */
 	Object execute(HashMap<String, Object> req, ZwaveRequest zwaveCommonRequest, boolean b);
 
 	
