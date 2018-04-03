11c69cf5075427821a15f4fb7131b6bb8c72753e
hyocheolahn <hcahn@hdtel.co.kr>
2018-04-03 13:38:42 +0900

    Package:Apply the ant build for the HT Connected Home Service Server.
    Change-Id: I378929e95b2f14cdd3339c7c21405f932c6e91ed

---
+++ package/iot-connected-home-server.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+#------------------------------------------------------------------------------
+# HT Connected Home Service Server start/stop script 
+#------------------------------------------------------------------------------
+# programmer : hyocheol ahn
+#------------------------------------------------------------------------------
+# start date : 2014/03/31
+#------------------------------------------------------------------------------
+
+# program directory
+root_dir="./"
+program="iot.connected.home.server"
+program_name="HTConnectedHomeServer"
+
+jar_list="ht-iot-connected-home-backend-server-*.jar"
+
+logfile="$root_dir/serverdog.log"
+this_script="iot-connected-home-server.sh"
+JAVA="java -Xmx256m"
+
+# get process pid
+getpids(){
+	pids=`ps -Af | grep "$1" | grep -v grep | sed -e 's/^........ *//' -e 's/ .*//'`
+	echo "$pids"
+}
+
+# start function
+start() {
+	# Start daemons.
+	pids=`getpids $program`
+	if [ "$pids" != "" ]; then
+		echo "$program_name: already running"
+  else
+    echo "$program_name: Starting..."
+    ulimit -c unlimited
+    ulimit -n 65530
+    $JAVA -jar $jar_list $program &
+	fi
+}
+
+# stop function
+stop() {
+	# Stop daemons.
+	pids=`getpids $program`
+	if [ "$pids" != "" ]; then
+  	echo "$program_name : Stopping... "
+  	kill $pids
+  fi
+}
+
+# status function
+status() {
+	pids=`getpids $program`
+	if [ "$pids" != "" ]; then
+		echo "$program_name: running(pid:$pids)"
+  else
+		echo "$program_name: not running"
+	fi
+	
+	pids=`getpids "$this_script watchdog"`
+	if [ "$pids" != "" ]; then
+		echo "watchdog: running(pid:$pids)"
+  else
+		echo "watchdog: not running"
+	fi
+}
+
+start_program(){
+	d=`date +%Y/%m/%d_%H:%M:%S`
+
+  echo "[$d] $1: Starting..." >> $logfile
+  ulimit -c unlimited
+  ulimit -n 65534
+  $JAVA -jar $jar_list $program &
+}
+
+watchdog(){	
+	echo "watchdog: Starting..."
+	sleep 60
+	while [ 1 ]; do		
+		pids=`getpids $program`
+		if [ "$pids" = "" ]; then
+			start_program
+		fi
+		
+		sleep 10
+	done
+}
+
+# watchdog start
+watchdog_start(){
+	$root_dir/$this_script watchdog &
+}
+
+# watchdog stop
+watchdog_stop(){
+	pids=`getpids "$this_script watchdog"`
+	for p in $pids
+	do
+		kill $p
+	done
+	echo "watchdog : Stopped"
+}
+
+# main function.
+case "$1" in
+	start)
+		start
+		watchdog_start
+		;;
+	stop)
+		watchdog_stop
+		stop
+		;;
+	status)
+		status
+		;;
+	restart)
+		stop
+		sleep 10
+		start
+		;;
+	watchdog)
+		watchdog
+		;;
+	*)
+		echo $"Usage: $0 {start|stop|status|restart}"
+		exit 1
+		;;
+esac
+
