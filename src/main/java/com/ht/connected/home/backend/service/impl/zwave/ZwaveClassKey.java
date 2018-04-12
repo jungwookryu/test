08a6fbc46d2a6ecdc19502b7dbc1b30a4f5456a2
injeongLee <ijlee@hdtel.co.kr>
2018-04-12 16:13:43 +0900

    static key 추후 properties로 바꿀예정

---
+++ src/main/java/com/ht/connected/home/backend/service/impl/zwave/ZwaveClassKey.java
@@ -0,0 +1,36 @@
+package com.ht.connected.home.backend.service.impl.zwave;
+
+public class ZwaveClassKey {
+
+	public static String NETWORK_MANAGEMENT_PROXY = "0x52";
+	public static String NETWORK_MANAGEMENT_BASIC = "0x4D";
+	public static String NETWORK_MANAGEMENT_INCLUSION = "0x34";
+	public static String NETWORK_MANAGEMENT_PRIMARY = "0x54";
+	public static String ZWAVE_PLUS_INFO = "0x5E";
+	public static String MANUFACTURER_SPECIFIC = "0x72";
+	public static String APPLICATION_STATUS = "0x22";
+	public static String MULTI_CHANNEL = "0x60";
+	public static String MULTI_CHANNEL_ASSOCIATION = "0x8E";
+	public static String DOOR_LOCK = "0x62";
+	public static String SECURITY = "0x98";
+	public static String SECURITY_2 = "0x9F";
+	public static String BASIC = "0x20";
+	public static String USER_CODE = "0x63";
+	public static String THERMOSTAT_MODE = "0x40";
+	public static String THERMOSTAT_SET_POINT = "0x43";
+	public static String THERMOSTAT_SETBACK = "0x47";
+	public static String NOTIFICATION = "0x71";
+	public static String CRC_16_ENCAPSULATION = "0x56";
+	public static String METER = "0x32";
+	public static String SENSOR_MULTILEVEL = "0x31";
+	public static String ASSOCIATION = "0x85";
+	public static String ASSOCIATION_GRP_INFO = "0x59";
+	public static String BATTERY = "0x80";
+	public static String SWITCH_BINARY = "0x25";
+	public static String DEVICE_RESET_LOCALLY = "0x5A";
+	public static String SWITCH_MULTILEVEL = "0x26";
+	public static String POWER_LEVEL = "0x73";
+	public static String VERSION = "0x86";
+	public static String WAKE_UP = "0x84";
+
+}
