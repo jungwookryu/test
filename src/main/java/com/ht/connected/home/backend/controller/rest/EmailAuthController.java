e4952ea31174c2b6a6f038e4623d71e0ba02f056
injeongLee <ijlee@hdtel.co.kr>
2018-04-17 15:09:31 +0900

    optimization import
    Change-Id: I53e5a8165e908f031254e8fed3a1a6cb892184b6

--- src/main/java/com/ht/connected/home/backend/controller/rest/EmailAuthController.java
+++ src/main/java/com/ht/connected/home/backend/controller/rest/EmailAuthController.java
@@ -1,10 +1,5 @@
 package com.ht.connected.home.backend.controller.rest;
 
-import com.ht.connected.home.backend.model.dto.UserActive;
-import com.ht.connected.home.backend.model.entity.Users;
-import com.ht.connected.home.backend.service.UsersService;
-
-import java.util.Date;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ht.connected.home.backend.model.dto.UserActive;
+import com.ht.connected.home.backend.model.entity.Users;
+import com.ht.connected.home.backend.service.UsersService;
 
 @RestController
 public class EmailAuthController extends CommonController {
