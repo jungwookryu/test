b809e83693c82830bb44bc51d42f29d1ab883db1
ht-jhkoo <jhkoo@hdtel.co.kr>
2018-04-25 16:10:26 +0900

    InputStream으로 회원가입 완료 html 파일 로드 후 응답

--- src/main/java/com/ht/connected/home/backend/controller/rest/EmailAuthController.java
+++ src/main/java/com/ht/connected/home/backend/controller/rest/EmailAuthController.java
@@ -1,6 +1,12 @@
 package com.ht.connected.home.backend.controller.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,25 +18,25 @@ import com.ht.connected.home.backend.service.UsersService;
 @Controller
 public class EmailAuthController extends CommonController {
 
-	UsersService usersService;
+    UsersService usersService;
 
-	@Autowired
-	public EmailAuthController(UsersService usersService) {
-		this.usersService = usersService;
-	}
+    @Autowired
+    public EmailAuthController(UsersService usersService) {
+        this.usersService = usersService;
+    }
 
-
-	@GetMapping("/adduser")
-	public Object addAuthUser(@RequestParam(value = "user_email", required=true) String userEmail ,
-															  @RequestParam(value = "redirected_code", required=true) String redirected_code
-			) {
-		Users rtnUsers = usersService.getUser(userEmail);
-		if (null!=rtnUsers) {
-			if(redirected_code.equals(rtnUsers.getRedirectiedCode())){
-				rtnUsers.setActive(UserActive.EMAIL_AUTH.ordinal());
-				usersService.modify(rtnUsers.getNo(), rtnUsers);
-			}
-		}
-		return "email/userRegisterConfirm";
-	}
+    @GetMapping("/adduser")
+    public ResponseEntity<String> addAuthUser(@RequestParam(value = "user_email", required = true) String userEmail,
+            @RequestParam(value = "redirected_code", required = true) String redirected_code) throws IOException {
+        Users rtnUsers = usersService.getUser(userEmail);
+        if (null != rtnUsers) {
+            if (redirected_code.equals(rtnUsers.getRedirectiedCode())) {
+                rtnUsers.setActive(UserActive.EMAIL_AUTH.ordinal());
+                usersService.modify(rtnUsers.getNo(), rtnUsers);
+            }
+            InputStream file =getClass().getClassLoader().getResourceAsStream("templates/email/userRegisterConfirm.html");
+            return new ResponseEntity(IOUtils.toString(file, "UTF-8"), HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
 }
