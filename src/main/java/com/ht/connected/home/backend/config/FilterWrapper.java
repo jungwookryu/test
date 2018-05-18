44dfa336a4f53ecee02f7a5e29484e40bdfaae99
injeongLee <ijlee@hdtel.co.kr>
2018-05-18 13:45:09 +0900

    fillter 삭제 및 login 비밀번호 가 틀렸을경우 사용자 권한 없음에 대한 노티를 줌 Change-Id: I7b6a0ce3e62a7c1f23db823ee5c7820313cbbcb3

--- src/main/java/com/ht/connected/home/backend/config/FilterWrapper.java
+++ src/main/java/com/ht/connected/home/backend/config/FilterWrapper.java
@@ -6,19 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FilterWrapper {
-    
+
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-	/**
+
+    /**
      * Returns a wrapper around the specified filter.
      */
-    static FilterWrapper wrap(Filter filter){
+    static FilterWrapper wrap(Filter filter) {
         return new FilterWrapper(filter);
     }
 
     private final Filter filter;
 
-    FilterWrapper(Filter filter){ 
-    logger.debug(filter.toString());
+    FilterWrapper(Filter filter) {
+        logger.debug("filter:::::::" + filter.toString());
         this.filter = filter;
     }
 
--- src/main/java/com/ht/connected/home/backend/config/security/FilterConfig.java
+++ src/main/java/com/ht/connected/home/backend/config/security/FilterConfig.java
@@ -1,6 +1,5 @@
 package com.ht.connected.home.backend.config.security;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -14,12 +13,5 @@ public class FilterConfig {
         characterEncodingFilter.setForceEncoding(true);
         return characterEncodingFilter;
     }
-    
-    @Bean
-    public FilterRegistrationBean getFilterRegistrationBean()
-    {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean(new HtFilter());
-        // registrationBean.addUrlPatterns("/*"); // 서블릿 등록 빈 처럼 패턴을 지정해 줄 수 있다.
-        return registrationBean;
-    }
+
 }
