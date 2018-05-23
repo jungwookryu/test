ad6abcb60fb47fef31b624dd86fee074edbce5ab
injeongLee <ijlee@hdtel.co.kr>
2018-05-24 08:27:56 +0900

    component anotaion loading 추가 Change-Id: I08e6910a2d3b76cfbc9333bc01a7e05ad28acc06

--- src/main/java/com/ht/connected/home/backend/HtConnectedHomeServerApplication.java
+++ src/main/java/com/ht/connected/home/backend/HtConnectedHomeServerApplication.java
@@ -2,11 +2,16 @@ package com.ht.connected.home.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.ht.connected.home.backend")
+@EnableJpaRepositories("com.ht.connected.home.backend")
+@EntityScan("com.ht.connected.home.backend")
 public class HtConnectedHomeServerApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(HtConnectedHomeServerApplication.class, args);
 	}
