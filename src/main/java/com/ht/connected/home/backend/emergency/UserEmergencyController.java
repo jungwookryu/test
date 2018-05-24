df167ab4b91ddfc0a4b631adc5efd523587b4d63
injeongLee <ijlee@hdtel.co.kr>
2018-05-24 10:30:23 +0900

    방범 생성 조회 구현
    Change-Id: Ie191097043f349adf0c6f23220f800aca103f900

---
+++ src/main/java/com/ht/connected/home/backend/emergency/UserEmergencyController.java
@@ -0,0 +1,93 @@
+package com.ht.connected.home.backend.emergency;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ht.connected.home.backend.controller.rest.CommonController;
+
+@RestController
+@RequestMapping("/emergency")
+public class UserEmergencyController extends CommonController {
+
+    UserEmergencyService userEmergencyService;
+
+    @Autowired
+    public UserEmergencyController(UserEmergencyService userEmergencyService) {
+        this.userEmergencyService = userEmergencyService;
+    }
+
+    /**
+     * 201, 204, 500, 406
+     * @param users
+     * @return
+     * @throws UnsupportedEncodingException
+     * @throws IllegalArgumentException
+     * @throws NoSuchAlgorithmException
+     */
+    @PostMapping
+    public ResponseEntity createUserEmergency(@RequestBody UserEmergency userEmergency, HttpServletRequest request) throws IllegalArgumentException, UnsupportedEncodingException {
+        userEmergency.setUserEmail(getAuthUserEmail());
+        userEmergency.setCreatedTime(new Date());
+        userEmergency.setLastModifiedTime(new Date());
+        UserEmergency rtnUserEmergencys = userEmergencyService.register(userEmergency);
+        logger.debug("rtnUserEmergencys:" + rtnUserEmergencys.toString());
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<HashMap<String, List>> getUserEmergencys() {
+        String authUserEmail = getAuthUserEmail();
+        List<UserEmergency> rtnUserEmergencys = userEmergencyService.getUserEmergency(authUserEmail);
+        HashMap map = new HashMap();
+        map.put("userEmergencys", new ArrayList());
+        if (rtnUserEmergencys.size() > 0) {
+            map.replace("userEmergencys", rtnUserEmergencys);
+        }
+        return new ResponseEntity(map, HttpStatus.OK);
+    }
+
+    @GetMapping("/{gatewayNo}")
+    public ResponseEntity<HashMap<String, UserEmergency>> getUserEmergency(@PathVariable("gatewayNo") int gatewayNo) throws IllegalArgumentException, UnsupportedEncodingException {
+        String authUserEmail = getAuthUserEmail();
+        List<UserEmergency> rtnUserEmergencys = userEmergencyService.getUserEmergency(authUserEmail, gatewayNo);
+        HashMap map = new HashMap();
+        map.put("userEmergencys", new ArrayList());
+        if (rtnUserEmergencys.size() > 0) {
+            map.replace("userEmergencys", rtnUserEmergencys);
+        }
+        return new ResponseEntity(map, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{no}")
+    public ResponseEntity<HttpStatus> deleteUserEmergency(@PathVariable("no") int no) {
+        userEmergencyService.delete(no);
+        return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{no}")
+    public ResponseEntity<HashMap<String, UserEmergency>> modifyUser(@PathVariable("no") int no, @RequestBody UserEmergency userEmergency) {
+        HashMap<String, UserEmergency> map = new HashMap<>();
+        UserEmergency rtnUserEmergency = userEmergencyService.modify(no, userEmergency);
+        map.put("rtnUserEmergency", rtnUserEmergency);
+        return new ResponseEntity<HashMap<String, UserEmergency>>(map, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/ht/connected/home/backend/emergency/UserEmergencyRepository.java
@@ -0,0 +1,26 @@
+/**
+ * Project : ht-connected-home-backend-server
+ * Package : com.ht.connected.home.backend.repository
+ * File : UsersRepository.java
+ * Description : 
+ * @auther : COM
+ * @version : 1.0
+ * @since : 2018. 2. 19.
+ */
+package com.ht.connected.home.backend.emergency;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author ijlee
+ *
+ */
+@Repository
+public interface UserEmergencyRepository extends JpaRepository<UserEmergency, Integer> {
+    List<UserEmergency> findByUserEmailAndGatewayNo(String authUserEmail, int gatewayNo);
+    List<UserEmergency> findByUserEmail(String authUserEmail);
+    int deleteByUserEmailAndGatewayNo(String authUserEmail, int gatewayNo);
+}
