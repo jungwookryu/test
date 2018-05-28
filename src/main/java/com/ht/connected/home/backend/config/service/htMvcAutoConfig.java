9ce8a19e336f1e7534c4b0e4cea17441d918cb93
injeongLee <ijlee@hdtel.co.kr>
2018-05-28 10:33:07 +0900

    Bean 생성 주기 관련 오류 수정함. 업무별 package 화 할 경우 controller가 config 전에 생성되므로 bean 을 찾지 못하는 오류가 생김
    Change-Id: I933ca55310e63cb908f920bb225ccf53ef258b10

--- src/main/java/com/ht/connected/home/backend/config/service/htMvcAutoConfig.java
+++ src/main/java/com/ht/connected/home/backend/config/service/htMvcAutoConfig.java
@@ -1,5 +1,25 @@
 package com.ht.connected.home.backend.config.service;
 
-public class htMvcAutoConfig {
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan("com.ht.connected.home.backend")
+@EnableJpaRepositories("com.ht.connected.home.backend")
+@EntityScan("com.ht.connected.home.backend")
+@AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
+public class htMvcAutoConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+		resourceHandlerRegistry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/**");
+		super.addResourceHandlers(resourceHandlerRegistry);
+	}
 
 }
