0e665b2319480b42e4e4a06313e3029d9ef4ce01
injeongLee <ijlee@hdtel.co.kr>
2018-07-04 15:51:41 +0900

    같은 객체에 대한 분기를 else if 로 수정됨. Change-Id: I30452c9703cc1bcb72ec41d96e4b5e6919e43da9

--- src/main/java/com/ht/connected/home/backend/controller/mqtt/ConsumerListener.java
+++ src/main/java/com/ht/connected/home/backend/controller/mqtt/ConsumerListener.java
@@ -20,21 +20,23 @@ public class ConsumerListener {
     
     
     public <T> void receiveMessage(T message) {
-        // public void onMessage(CompleteMessageCorrelationData message) {
         if (message instanceof String) {
             logger.info("onMessageReceived < " + message + " >");
         }
-        if (message instanceof Message) {
+        else if (message instanceof Message) {
             logger.info("onMessageReceived < " + message.toString() + " >");
         }
-        if (message instanceof List) {
+        else if (message instanceof List) {
             logger.info("onMessageReceived < " + ((List) message).size() + " >");
         }
         if (message instanceof JSONObject) {
             logger.info("onMessageReceived < " + ((JSONObject) message).toString() + " >");
         }
-        if (message instanceof byte[]) {
+        else if (message instanceof byte[]) {
             logger.info("onMessageReceived < " + ((byte[]) message).toString() + " >");
+        }
+        else if (message instanceof Object) {
+            logger.info("onMessageReceived < " + message.toString() + " >");
         }
         latch.countDown();
     }
