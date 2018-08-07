68415e6b2777dae9d8c07c62cb7623e2127a8104
injeongLee <ijlee@hdtel.co.kr>
2018-08-07 09:38:48 +0900

    gateway certi_key 컬럼추가 share_home nick name 컬럼 삭제 home 생성 service로 구조화
    Change-Id: Ic6a88c002e606a074de0bd109cef96b4f4a7d249

--- src/main/java/com/ht/connected/home/backend/client/home/HomeController.java
+++ src/main/java/com/ht/connected/home/backend/client/home/HomeController.java
@@ -16,9 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ht.connected.home.backend.client.home.HomeServiceImpl.Share;
-import com.ht.connected.home.backend.client.home.HomeServiceImpl.ShareRole;
-import com.ht.connected.home.backend.client.home.HomeServiceImpl.Status;
-import com.ht.connected.home.backend.client.home.sharehome.ShareHome;
 import com.ht.connected.home.backend.client.user.User;
 import com.ht.connected.home.backend.client.user.UserService;
 import com.ht.connected.home.backend.common.Common;
@@ -26,64 +23,66 @@ import com.ht.connected.home.backend.controller.rest.CommonController;
 
 /**
  * Home(호스트)관련 요청 처리
+ * 
  * @author ijlee
  */
 @RestController
 @RequestMapping("/home")
 public class HomeController extends CommonController {
 
-    HomeService homeService;
-    UserService userService;
+	HomeService homeService;
+	UserService userService;
 
-    @Autowired
-    public HomeController(HomeService homeService,
-            UserService userService) {
-        this.homeService = homeService;
-        this.userService = userService;
-    }
+	@Autowired
+	public HomeController(HomeService homeService, UserService userService) {
+		this.homeService = homeService;
+		this.userService = userService;
+	}
 
-    /**
-     * 로그인한 사용자가 등록한 모든 Home를 조회한다.
-     * @param req
-     * @return
-     * @throws Exception
-     */
-    @GetMapping
-    public ResponseEntity<HashMap<String, Object>> getHomeList(@RequestParam(value = "nickname", required = false) String nickname)
-            throws Exception {
-        String authUserEmail = getAuthUserEmail();
-        List<Home> homes = homeService.getHomeListByEmail(authUserEmail, nickname);
-        Home rtnHome = new Home();
-        rtnHome.setHomes(homes);
-        return new ResponseEntity(rtnHome, HttpStatus.OK);
-    }
-    
-    /**
-     * 
-     * @param map
-     * @return
-     */
-    @PutMapping
-    public ResponseEntity shareHome(@RequestBody HashMap map) {
-        String userEmail = getAuthUserEmail();
-        int no = (int) map.getOrDefault("home_no",-1);
-        String share_user_email = (String) map.getOrDefault("share_user_email","");
-        String mode = (String) map.getOrDefault("mode","");
-        Home originHome = homeService.findOne(no);
-        User user =userService.getUser(share_user_email);
-        if(Common.empty(share_user_email)) {
-        	return new ResponseEntity<>("requestUser not found", HttpStatus.NOT_ACCEPTABLE);
-        }
-        if((-1 == no) || Objects.isNull(originHome)) {
-        	return new ResponseEntity<>("original home not found", HttpStatus.NOT_ACCEPTABLE);
-        }
-        if((!userEmail.equals(originHome.getOwnerUserEmail()))) {
-        	return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-        }
-        if(Objects.isNull(user)) {
-            return new ResponseEntity<>("shareUser not found", HttpStatus.NOT_ACCEPTABLE);
-        }
-        boolean rtnShareHome = false;
+	/**
+	 * 로그인한 사용자가 등록한 모든 Home를 조회한다.
+	 * 
+	 * @param req
+	 * @return
+	 * @throws Exception
+	 */
+	@GetMapping
+	public ResponseEntity<HashMap<String, Object>> getHomeList(
+			@RequestParam(value = "nickname", required = false) String nickname) throws Exception {
+		String authUserEmail = getAuthUserEmail();
+		List<Home> homes = homeService.getHomeListByEmail(authUserEmail, nickname);
+		Home rtnHome = new Home();
+		rtnHome.setHomes(homes);
+		return new ResponseEntity(rtnHome, HttpStatus.OK);
+	}
+
+	/**
+	 * 
+	 * @param map
+	 * @return
+	 */
+	
+	@PutMapping
+	public ResponseEntity shareHome(@RequestBody HashMap map) {
+		String userEmail = getAuthUserEmail();
+		int no = (int) map.getOrDefault("home_no", -1);
+		String share_user_email = (String) map.getOrDefault("share_user_email", "");
+		String mode = (String) map.getOrDefault("mode", "");
+		Home originHome = homeService.findOne(no);
+		User user = userService.getUser(share_user_email);
+		if (Common.empty(share_user_email)) {
+			return new ResponseEntity<>("requestUser not found", HttpStatus.NOT_ACCEPTABLE);
+		}
+		if ((-1 == no) || Objects.isNull(originHome)) {
+			return new ResponseEntity<>("original home not found", HttpStatus.NOT_ACCEPTABLE);
+		}
+		if ((!userEmail.equals(originHome.getOwnerUserEmail()))) {
+			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+		}
+		if (Objects.isNull(user)) {
+			return new ResponseEntity<>("shareUser not found", HttpStatus.NOT_ACCEPTABLE);
+		}
+		boolean rtnShareHome = false;
 		// 공유
 		if (Share.share.name().equals(mode)) {
 			rtnShareHome = homeService.shareHome(originHome, user);
@@ -96,25 +95,27 @@ public class HomeController extends CommonController {
 		if (Share.masterModify.name().equals(mode)) {
 			rtnShareHome = homeService.masterModifyHome(originHome, user);
 		}
-        if(rtnShareHome) {
-            return new ResponseEntity<>(rtnShareHome,HttpStatus.OK);
-        }else {
-            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
-        }
+		if (rtnShareHome) {
+			return new ResponseEntity<>(rtnShareHome, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+		}
 
-    }
-    
-    /**
-     * 로그인한 사용자가 등록한 모든 Home를 조회한다.
-     * @param req
-     * @return
-     * @throws Exception
-     */
-    @PostMapping
-    public ResponseEntity<HashMap<String, Object>> registHome(@RequestBody Home requestHome) throws Exception {
-        String authUserEmail = getAuthUserEmail();
-        Home rtnHome = homeService.createHome(requestHome);
-        return new ResponseEntity(rtnHome, HttpStatus.OK);
-    }    
-    
+	}
+
+	/**
+	 * 로그인한 사용자가 등록한 모든 Home를 조회한다.
+	 * 
+	 * @param req
+	 * @return
+	 * @throws Exception
+	 */
+	@PostMapping
+	public ResponseEntity<HashMap<String, Object>> registHome(@RequestBody Home requestHome) throws Exception {
+		String authUserEmail = getAuthUserEmail();
+		requestHome.setOwnerUserEmail(authUserEmail);
+		Home rtnHome = homeService.createHome(requestHome);
+		return new ResponseEntity(rtnHome, HttpStatus.OK);
+	}
+
 }
